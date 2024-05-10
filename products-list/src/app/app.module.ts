450e34cccd3f40b9d30cabeffcc2d6551a921698
Mozart Soares <mozartarmstrong@gmail.com>
2024-05-09 23:33:35 -0300

    fix: styles fix in components

--- products-list/src/app/app.module.ts
+++ products-list/src/app/app.module.ts
@@ -8,9 +8,17 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NewItemComponent } from './components/pages/new-item/new-item.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, NewItemComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NewItemComponent,
+    TableComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
