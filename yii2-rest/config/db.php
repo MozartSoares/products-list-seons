e1c2991ce311030c9c123779a0c6d38b9522f136
Mozart Soares <mozartarmstrong@gmail.com>
2024-05-11 18:46:20 -0300

    add: create migration and set database

--- yii2-rest/config/db.php
+++ yii2-rest/config/db.php
@@ -2,13 +2,24 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=products-db',
+    'dsn' => 'mysql:host=localhost;',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
+    'on afterOpen' => function($event) {
+        $db = $event->sender;
+        $dbName = 'products-db';
 
+        // Criar o banco de dados se ainda não existir
+        $command = $db->createCommand("CREATE DATABASE IF NOT EXISTS `$dbName`");
+        $command->execute();
+
+        // Usar o banco de dados recém-criado
+        $db->createCommand("USE `$dbName`")->execute();
+    },
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
 ];
+
